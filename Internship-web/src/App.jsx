e48efe72ea792e338fbb610facebd0cf57b8fe83
BODA <mmmmhe822005@gmail.com>
2025-05-02 09:50:30 +0300

    Initial setup project
    Initial setup of react project

---
+++ Internship-web/src/App.jsx
@@ -0,0 +1,15 @@
+// App.jsx
+import React, { useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+
+const App = () => {
+  
+  return (
+    <Routes>
+      
+    </Routes>
+  );
+};
+
+export default App;
